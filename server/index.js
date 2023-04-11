0b8c5e70db3c9477c4bcc68f540e8d0ead7e7c1c
Ryan Murphy <murjax@gmail.com>
2023-04-11 19:34:49 -0400

    Use port 3001 for server

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const db = require('./queries')
 const app = express()
-const port = 3000
+const port = 3001
 
 app.use(bodyParser.json())
 app.use(
