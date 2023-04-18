57e9607ff8b3a93f014fcb9ce8d1c0b8b0150cf1
Gregory Boyd <gboyd69@yahoo.com>
2023-04-18 14:02:27 -0400

    added responsive characteristics and basic styling to react components.

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -3,19 +3,36 @@ import "../styles/styles.css";
 
 function Main() {
   const [isMale, setIsMale] = useState(true);
-  useEffect(() => {}, [isMale]);
+  useEffect(() => {
+    console.log(isMale);
+  }, [isMale]);
 
   return (
     <main>
       <h2>A tool to randomly generate a dog or cat name!</h2>
       <div className="btn-genders">
-        <button type="button">Male</button>
-        <button type="button">Female</button>
+        <button
+          type="button"
+          onClick={() => {
+            setIsMale(true);
+          }}
+          className={isMale ? "custom-button active" : "custom-button"}
+        >
+          Male
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            setIsMale(false);
+          }}
+          className={isMale ? "custom-button" : "custom-button active"}
+        >
+          Female
+        </button>
       </div>
       <div className="btn-generate">
         <button type="button">Generate</button>
       </div>
-
       <div className="card-container">
         <h3>Generated name</h3>
       </div>
