313bc2a6c481b086e7a37375daaccc033718b639
Gregory Boyd <gboyd69@yahoo.com>
2023-04-18 13:00:37 -0400

    removed boilerplate react icons added css files and basic responsive layout

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,14 +1,13 @@
 import React from "react";
 import petlogo from "../images/petlogo.jpg";
+import "../styles/header.css";
 
 function Header() {
   return (
-    <div className="header">
-      <h1>
-        <img src={petlogo} alt="dog and cat outline" />
-        Pet Name Generator
-      </h1>
-    </div>
+    <header>
+      <img src={petlogo} alt="dog and cat outline" />
+      <h1>Pet Name Generator</h1>
+    </header>
   );
 }
 
