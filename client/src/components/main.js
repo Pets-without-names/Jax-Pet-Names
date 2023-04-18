ccb0ba4a10bc056f6db2d465cf33a78e6d660c92
Gregory Boyd <gboyd69@yahoo.com>
2023-04-18 13:00:37 -0400

    removed boilerplate react icons added css files and basic responsive layout

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "../styles/styles.css";
 
 function Main() {
-  // Event handlers:
+  const [isMale, setIsMale] = useState(true);
+  useEffect(() => {}, [isMale]);
+
   return (
-    <div>
+    <main>
       <h2>A tool to randomly generate a dog or cat name!</h2>
-      <button type="button">Male</button>
-      <button type="button">Female</button>
-      <div>
+      <div className="btn-genders">
+        <button type="button">Male</button>
+        <button type="button">Female</button>
+      </div>
+      <div className="btn-generate">
         <button type="button">Generate</button>
       </div>
-      <div>
-        <span>Name Result</span>
+
+      <div className="card-container">
+        <h3>Generated name</h3>
       </div>
-    </div>
+    </main>
   );
 }
 
