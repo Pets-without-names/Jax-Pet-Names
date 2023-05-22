737d5221c5cfce1ae0a8ae12370aa255e82fea48
Gregory Boyd <gboyd69@yahoo.com>
2023-05-22 11:37:59 -0400

    added addName.js file to allow the user to add a pet name to the database

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import '../src/styles/main.css';
 import Header from './components/header';
 import Main from './components/main';
 import Footer from './components/footer';
+import AddName from './components/addName';
 
 function App() {
   return (
     <div>
       <Header></Header>
       <Main></Main>
+      <AddName></AddName>
       <Footer></Footer>
     </div>
   );
