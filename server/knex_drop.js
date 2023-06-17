10a19c2c30a7dc74df7d4203e8b9306e6151e5a2
Ryan Murphy <murjax@gmail.com>
2023-06-17 19:50:09 -0400

    Create Dockerfile and docker-compose for project

--- server/knex_drop.js
+++ server/knex_drop.js
@@ -3,17 +3,11 @@ const environment = process.env.ENVIRONMENT || 'development'
 const config = {
   development: {
     client: 'postgresql',
-    connection: {
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-    },
+    connection: process.env.DB_SETUP_URL,
   },
   staging: {
     client: 'postgresql',
-    connection: {
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-    },
+    connection: process.env.DB_SETUP_URL,
   },
   production: {
     client: 'postgresql',
@@ -22,4 +16,7 @@ const config = {
 };
 const knex = require('knex')(config[environment]);
 
-knex.raw('DROP DATABASE pet_names;').then(() => process.exit(0));
+knex
+  .raw('DROP DATABASE pet_names;')
+  .catch((error) => console.log('Unable to drop database pet_names. Proceeding assuming it does not exist yet.'))
+  .finally(() => process.exit(0));
