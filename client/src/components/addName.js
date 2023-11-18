f35b2e57d856e9ac99d26a2f522fb8621c60929c
Gregory Boyd <gboyd69@yahoo.com>
2023-11-17 19:40:39 -0500

    rearranged code order

--- client/src/components/addName.js
+++ client/src/components/addName.js
@@ -45,6 +45,17 @@ function AddName() {
     }
   };
 
+  const onError = (errors) => {
+    console.log('error: ' + errors);
+    //Other error handling code:
+  };
+
+  const closeModal = (event) => {
+    setIsOpen(false);
+    setUniqueError(false);
+    reset();
+  };
+
   const onSubmit = (formData) => {
     // POST API call
     fetch(`${process.env.REACT_APP_HOST}/names`, {
@@ -74,17 +85,6 @@ function AddName() {
       .finally(
         reset() //clears/resets the form values
       );
-  };
-
-  const onError = (errors) => {
-    console.log('error: ' + errors);
-    //Other error handling code:
-  };
-
-  const closeModal = (event) => {
-    setIsOpen(false);
-    setUniqueError(false);
-    reset();
   };
 
   return (
