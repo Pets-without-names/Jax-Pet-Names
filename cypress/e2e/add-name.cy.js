61ab3bcc06f47d64a449e322c233fe112e9edde5
Gregory Boyd <gboyd69@yahoo.com>
2023-11-26 12:40:46 -0500

    added cy e2e testing for adding a name

--- cypress/e2e/add-name.cy.js
+++
@@ -1,7 +0,0 @@
-describe('Adding a name to the database', () => {
-  const environment = ['localhost:3000'];
-  it('adds a name in the input field then clicks the add button', () => {
-    cy.visit(`http://${environment}`)
-    
-  })
-})
