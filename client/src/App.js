29fbb27567d6ebf549a4bf5057d23b33a3a0ab5c
Gregory Boyd <gboyd69@yahoo.com>
2024-01-06 14:47:22 -0500

    added google ananlytics4 code to app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,11 @@ import Header from './components/header';
 import Main from './components/main';
 import Footer from './components/footer';
 import AddName from './components/addName';
+import ReactGA from 'react-ga4';
+
+const measurementID = 'G-CG5SKK9RSW';
+ReactGA.initialize(measurementID);
+ReactGA.send({ hitType: 'pageview', page: window.location.pathname });
 
 function App() {
   return (
