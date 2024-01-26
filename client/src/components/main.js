f7e97deb87e5915b69f9136f4a685e5ddd012ced
Gregory Boyd <gboyd69@yahoo.com>
2024-01-25 21:13:30 -0500

    updated seo content

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -39,7 +39,7 @@ function Main() {
         <meta name='author' content='Petswithoutnames.org' />
         <meta
           name='description'
-          content='This website generates random male or female pet names.  Names 
+          content='This website randomly generates male or female pet names.  Names 
         may be added to the pet name database'
         />
         <meta name='robots' content='index follow' />
