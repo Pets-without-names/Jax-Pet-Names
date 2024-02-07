99fcf7e22f9ebcda0f1f2bae13e5390afb8f9a7e
Gregory Boyd <gboyd69@yahoo.com>
2024-02-06 19:31:53 -0500

    added meta og tags for social media

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -41,8 +41,8 @@ function Main() {
         <meta name='author' content='Petswithoutnames.org' />
         <meta
           name='description'
-          content='Randomly generate either male or female pet names.  You can help build a better product by
-          adding names to the pet name database'
+          content='Randomly generate either male or female pet names.  You can add your own names
+          to the pet name database'
         />
         <meta name='robots' content='index follow' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
